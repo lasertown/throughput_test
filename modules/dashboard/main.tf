581c9d2ab4aaecf2cff5d07ff6035582b025f93a
lasertown <jasonju@gmail.com>
2021-07-11 16:41:13 -0700

    Update main.tf

--- modules/dashboard/main.tf
+++ modules/dashboard/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_dashboard" "dashboard" {
   dashboard_properties = templatefile("${path.module}/dash_template/backends.tpl",
     {
       sub_id         = data.azurerm_subscription.current.subscription_id,
-      resource_group = var.rg
+      resource_group = var.rg,
       node           = var.node_name
   })
 }
