8d3f9a33a9d80978329875f23323417c32f7fa4a
lasertown <jasonju@gmail.com>
2021-07-10 17:11:53 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,9 @@ output "rg" {
   value = module.rg0.rg
 }
   
-output "node0" {
+output "node_public_ip" {
   value = module.node0.public_ip
+}
+output "node_name" {
+  value = module.node0.node_name
 }
