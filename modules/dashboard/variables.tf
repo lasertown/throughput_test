c5fea9f3607dc587b71f71aa36ffc570fd11a3ed
lasertown <jasonju@gmail.com>
2021-07-11 16:56:52 -0700

    Update variables.tf

--- modules/dashboard/variables.tf
+++ modules/dashboard/variables.tf
@@ -3,6 +3,10 @@ variable "rg" {
   description = "The Resource Group name for all resources in this module"
 }
 
+variable "region" {
+  description = "The Azure location where all resources in this example should be created"
+}
+
 variable "node_name" {
   description = "Name of the VM"
 }
