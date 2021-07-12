52592797eafff2aee5b68331b2896768b3fcf7c8
lasertown <jasonju@gmail.com>
2021-07-11 20:07:36 -0700

    Update main.tf

--- modules/node/main.tf
+++ modules/node/main.tf
@@ -70,3 +70,125 @@ resource "azurerm_linux_virtual_machine" "node" {
     group = var.tag
     }
 }
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_dashboard" "dashboard" {
+  name                = "Throughput"
+  resource_group_name = var.rg
+  location            = var.region
+  dashboard_properties = <<DASH
+  {
+    "lenses": {
+      "0": {
+        "order": 0,
+        "parts": {
+          "0": {
+            "position": {
+              "x": 0,
+              "y": 0,
+              "colSpan": 6,
+              "rowSpan": 4
+            },
+            "metadata": {
+              "inputs": [
+                {
+                  "name": "options",
+                  "value": {
+                    "chart": {
+                      "metrics": [
+                        {
+                          "resourceMetadata": {
+                            "id": "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.rg}/providers/Microsoft.Compute/virtualMachines/${var.node_name}"
+                          },
+                          "name": "Data Disk Read Bytes/sec",
+                          "aggregationType": 4,
+                          "namespace": "microsoft.compute/virtualmachines",
+                          "metricVisualization": {
+                            "displayName": "Data Disk Read Bytes/Sec"
+                          }
+                        }
+                      ],
+                      "title": "Avg Data Disk Read Bytes/Sec by LUN",
+                      "titleKind": 1,
+                      "visualization": {
+                        "chartType": 2,
+                        "legendVisualization": {
+                          "isVisible": true,
+                          "position": 2,
+                          "hideSubtitle": false
+                        },
+                        "axisVisualization": {
+                          "x": {
+                            "isVisible": true,
+                            "axisType": 2
+                          },
+                          "y": {
+                            "isVisible": true,
+                            "axisType": 1
+                          }
+                        }
+                      },
+                      "grouping": {
+                        "dimension": "LUN",
+                        "sort": 2,
+                        "top": 10
+                      },
+                      "timespan": {
+                        "relative": {
+                          "duration": 43200000
+                        },
+                        "showUTCTime": false,
+                        "grain": 2
+                      }
+                    }
+                  },
+                  "isOptional": true
+                },
+                {
+                  "name": "sharedTimeRange",
+                  "isOptional": true
+                }
+              ],
+              "type": "Extension/HubsExtension/PartType/MonitorChartPart"
+            }
+          }
+        }
+      }
+    },
+    "metadata": {
+      "model": {
+        "timeRange": {
+          "value": {
+            "relative": {
+              "duration": 24,
+              "timeUnit": 1
+            }
+          },
+          "type": "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
+        },
+        "filterLocale": {
+          "value": "en-us"
+        },
+        "filters": {
+          "value": {
+            "MsPortalFx_TimeRange": {
+              "model": {
+                "format": "utc",
+                "granularity": "auto",
+                "relative": "24h"
+              },
+              "displayCache": {
+                "name": "UTC Time",
+                "value": "Past 24 hours"
+              },
+              "filteredPartIds": []
+            }
+          }
+        }
+      }
+    }
+}
+DASH
+}
+
