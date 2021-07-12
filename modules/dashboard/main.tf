a5e396ceb5db66cf7e1a5ecc81ffc026d60f4a25
lasertown <jasonju@gmail.com>
2021-07-11 17:45:15 -0700

    Update main.tf

--- modules/dashboard/main.tf
+++ modules/dashboard/main.tf
@@ -6,6 +6,7 @@ data "azurerm_subscription" "current" {}
 
 variable "subid" {
   default = data.azurerm_subscription.current.subscription_id
+}
 
 resource "azurerm_dashboard" "dashboard" {
   name                = "throughput_lab"
