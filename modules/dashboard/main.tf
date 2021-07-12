861faf4eadc413f7ed762bad416f6fe0feaa92b5
lasertown <jasonju@gmail.com>
2021-07-11 17:44:21 -0700

    Update main.tf

--- modules/dashboard/main.tf
+++ modules/dashboard/main.tf
@@ -4,13 +4,16 @@ provider "azurerm" {
 
 data "azurerm_subscription" "current" {}
 
+variable "subid" {
+  default = data.azurerm_subscription.current.subscription_id
+
 resource "azurerm_dashboard" "dashboard" {
   name                = "throughput_lab"
   resource_group_name = var.rg
   location            = var.region
   dashboard_properties = templatefile("dash_template/dash.tpl",
     {
-      sub_id         = azurerm_subscription.current.subscription_id
+      sub_id         = var.subid,
       resource_group = var.rg,
       node           = var.node_name
   })
